413a122b1a6901d6354b5eb543f29bf8f7158a02
Maxim Poliakovski <maximumspatium@googlemail.com>
2019-04-22 21:04:44 +0200

    Update to Javacpp 1.5 and Tesseract 4.0.0.
    Incorporate necessary Javacpp 1.5 API changes.

--- src/main/org/audiveris/omr/text/tesseract/TesseractOCR.java
+++ src/main/org/audiveris/omr/text/tesseract/TesseractOCR.java
@@ -30,9 +30,10 @@ import org.audiveris.omr.text.TextChar;
 import org.audiveris.omr.text.TextLine;
 import org.audiveris.omr.text.TextWord;
 
-import org.bytedeco.javacpp.tesseract;
-import org.bytedeco.javacpp.tesseract.StringGenericVector;
-import org.bytedeco.javacpp.tesseract.TessBaseAPI;
+import org.bytedeco.tesseract.global.tesseract;
+import org.bytedeco.tesseract.StringGenericVector;
+import org.bytedeco.tesseract.TessBaseAPI;
+import static org.bytedeco.tesseract.global.tesseract.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -101,6 +102,11 @@ public class TesseractOCR
 
             try {
                 TessBaseAPI api = new TessBaseAPI();
+
+                // we need to call this to avoid crash
+                // in native code on non-english systems
+                // https://github.com/bytedeco/javacpp-presets/issues/694
+                setlocale(LC_ALL(), "C");
 
                 if (api.Init(ocrFolder.toString(), "eng") == 0) {
                     StringGenericVector languages = new StringGenericVector();
@@ -311,9 +317,9 @@ public class TesseractOCR
     private Path scanOcrLocations (String[] locations)
     {
         for (String loc : locations) {
-            final Path path = Paths.get(loc);
+            final Path path = Paths.get(loc).resolve("tessdata");
 
-            if (Files.exists(path.resolve("tessdata"))) {
+            if (Files.exists(path)) {
                 return path;
             }
         }
